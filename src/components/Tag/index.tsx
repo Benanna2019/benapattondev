6c5c5935de7a29433ef7e7ceab3d33d889413d32
Benanna2019 <bass41992ben@gmail.com>
2023-01-08 15:42:53 -0500

    fixed prose styles and udpated tags

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -29,7 +29,7 @@ export function Tag({ name }: any) {
       }
       case "open source": {
         specificClasses =
-          "border-green-200 text-green-600 bg-green-500 bg-opacity-30 hover:bg-opacity-60";
+          "border-green-200 text-green-600 bg-green-500 bg-opacity-60 hover:bg-opacity-90";
         break;
       }
       case "portfolio": {
@@ -44,112 +44,112 @@ export function Tag({ name }: any) {
       }
       case "reading": {
         specificClasses =
-          "border-green-200 text-green-600 bg-green-200 bg-opacity-30 hover:bg-opacity-60";
+          "border-green-200 text-green-600 bg-green-200 bg-opacity-60 hover:bg-opacity-90";
         break;
       }
       case "developer": {
         specificClasses =
-          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-30 hover:bg-opacity-60";
+          "border-gray-200 text-gray-600 bg-gray-100 bg-opacity-60 hover:bg-opacity-90";
         break;
       }
       case "developer relations": {
         specificClasses =
-          "border-fuchsia-200 text-fuchsia-600 bg-fuchsia-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-fuchsia-200 text-fuchsia-600 bg-fuchsia-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "developer experience": {
         specificClasses =
-          "border-fuchsia-200 text-fuchsia-600 bg-fuchsia-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-fuchsia-200 text-fuchsia-600 bg-fuchsia-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "technical": {
         specificClasses =
-          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "draft": {
         specificClasses =
-          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "guides": {
         specificClasses =
-          "border-sky-200 text-sky-600 bg-sky-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-sky-200 text-sky-600 bg-sky-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "part time creator": {
         specificClasses =
-          "border-orange-200 text-orange-600 bg-orange-200 bg-opacity-5  hover:bg-opacity-60";
+          "border-orange-200 text-orange-600 bg-orange-200 bg-opacity-5  hover:bg-opacity-90";
         break;
       }
       case "work": {
         specificClasses =
-          "border-cyan-200 text-cyan-600 bg-cyan-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-cyan-200 text-cyan-600 bg-cyan-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "productivity": {
         specificClasses =
-          "border-teal-200 text-teal-600 bg-teal-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-teal-200 text-teal-600 bg-teal-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "random thoughts": {
         specificClasses =
-          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "nextjs": {
         specificClasses =
-          "border-blue-200 text-blue-600 bg-blue-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-blue-200 text-blue-600 bg-blue-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "networking": {
         specificClasses =
-          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "junior developer": {
         specificClasses =
-          "border-lime-200 text-lime-600 bg-lime-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-lime-200 text-lime-600 bg-lime-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "javascript": {
         specificClasses =
-          "border-yellow-200 text-yellow-600 bg-yellow-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-yellow-200 text-yellow-600 bg-yellow-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "coding bootcamp": {
         specificClasses =
-          "border-indigo-200 text-indigo-600 bg-indigo-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-indigo-200 text-indigo-600 bg-indigo-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "career changer": {
         specificClasses =
-          "border-violet-200 text-violet-600 bg-violet-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-violet-200 text-violet-600 bg-violet-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "design patterns": {
         specificClasses =
-          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "react": {
         specificClasses =
-          "border-sky-200 text-sky-600 bg-sky-200 bg-opacity-30  hover:bg-opacity-60";
+          "border-sky-200 text-sky-700 bg-sky-200 bg-opacity-60  hover:bg-opacity-90";
         break;
       }
       case "svelte": {
         specificClasses =
-          "border-red-200 text-red-600 bg-red-200 bg-opacity-30 hover:bg-opacity-60";
+          "border-red-200 text-red-600 bg-red-200 bg-opacity-60 hover:bg-opacity-90";
         break;
       }
       case "serverless": {
         specificClasses =
-          "border-yellow-200 text-yellow-600 bg-yellow-300 bg-opacity-20 hover:bg-opacity-60";
+          "border-yellow-200 text-yellow-600 bg-yellow-300 bg-opacity-20 hover:bg-opacity-90";
         break;
       }
       case "__clear_tag_picker": {
         specificClasses =
-          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-30 hover:bg-opacity-40";
+          "border-gray-200 text-gray-600 bg-gray-200 bg-opacity-60 hover:bg-opacity-90";
         break;
       }
     }
